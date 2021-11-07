502465c1d082825558d05d246f4bbcf8020c18ca
William <williamhjang@gmail.com>
2021-11-07 15:04:03 -0500

    Updates

---
+++ precache-manifest.c2cc3c5835b376d7a45363b18364b410.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2839a5fcee5680bde293a791c632ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e7a630593a745edd57a",
+    "url": "/static/css/2.f00116b5.chunk.css"
+  },
+  {
+    "revision": "fd6894ee2f7f4973f66e",
+    "url": "/static/css/main.eedabf63.chunk.css"
+  },
+  {
+    "revision": "4e7a630593a745edd57a",
+    "url": "/static/js/2.a37ea600.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.a37ea600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd6894ee2f7f4973f66e",
+    "url": "/static/js/main.0d9684f5.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
